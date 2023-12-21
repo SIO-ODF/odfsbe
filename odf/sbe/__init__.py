1eba94e5daf3ffaa85f104cb16875745a39b6f13
Andrew Barna <abarna@gmail.com>
2023-12-21 12:07:41 -0600

    add hex_to_dataset and read_hex

---
+++ odf/sbe/__init__.py
@@ -0,0 +1,84 @@
+from typing import Literal
+from pathlib import Path
+
+import numpy as np
+import xarray as xr
+
+ERRORS = Literal["store", "raise", "ignore"]
+
+def hex_to_dataset(hex:str, errors: ERRORS="raise") -> xr.Dataset:
+    _comments = []  #   hex header comments written by deck box/SeaSave
+    out = []        #   hex bytes out
+
+    datalen = 0
+    for lineno, line in enumerate(hex.splitlines(), start=1):
+        if "number of bytes per scan" in line.lower():
+            datalen = int(line.split("= ")[1])
+            linelen = datalen * 2
+
+        if line.startswith("*"): # comment
+            _comments.append(line)
+            continue
+
+        if datalen == 0:
+            raise ValueError(f"Could not find number of bytes per scan in {lineno} lines")
+
+        if len(line) != linelen:
+            if errors == "raise":
+                raise ValueError(f"invalid scan lengths line: {lineno}")
+            elif errors == "ignore":
+                continue
+            elif errors == "store":
+                raise NotImplementedError("better figure out how to do this")
+
+        out.append([*bytes.fromhex(line)])
+    header = "\n".join(_comments)
+    data = np.array(out, dtype=np.uint8)
+
+    data_array = xr.DataArray(data, dims=["scan","bytes_per_scan"])
+    data_array.encoding["zlib"] = True  # compress the data
+    data_array.encoding["complevel"] = 6  # use compression level 6
+    data_array.encoding["chunksizes"] = (60*60*24, 1) # chunk every hour of data (for 24hz data), and each column seperately
+    # This is about 3~4mb chunks uncompressed depending on how many channels there are
+
+    return xr.Dataset({
+        "hex": data_array  # TODO: decide on the name of this variable as we will live with it "forever", "hex" is awful, but maybe the best?
+    },
+    attrs={
+        "hex_header": header
+    })
+
+def read_hex(path, xmlcon=(".XMLCON", ".xmlcon"), bl=(".bl", ), hdr=(".hdr", )) -> xr.Dataset:
+    hex_path = Path(path)
+    xmlcon_path = None
+    bl_path = None
+    hdr_path = None
+
+    if xmlcon is not None:
+        for suffix in xmlcon:
+            if (xmlcon_p := hex_path.with_suffix(suffix)).exists():
+                xmlcon_path = xmlcon_p
+
+    if bl is not None:
+        for suffix in bl:
+            if (bl_p := hex_path.with_suffix(suffix)).exists():
+                bl_path = bl_p
+    if hdr is not None:
+        for suffix in hdr:
+            if (hdr_p := hex_path.with_suffix(suffix)).exists():
+                hdr_path = hdr_p
+
+    ds = hex_to_dataset(hex_path.read_text(), errors="ignore")
+    ds.attrs["hex_filename"] = hex_path.name
+    if xmlcon_path is not None:
+        ds.attrs["xmlcon_filename"] = xmlcon_path.name
+        ds.attrs["xmlcon"] = xmlcon_path.read_text()
+    if bl_path is not None:
+        ds.attrs["bl_filename"] = bl_path.name
+        ds.attrs["bl"] = bl_path.read_text()
+
+    if hdr_path is not None:
+        ds.attrs["hdr_filename"] = hdr_path.name
+        # TODO... figure out why this file doesn't seem to be matching the hex header (line endings?), and what to do when it doesnt
+
+    return ds
