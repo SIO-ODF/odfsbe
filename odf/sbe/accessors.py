71336d25d0ba894c5187a553a708335675d4b56e
Andrew Barna <abarna@gmail.com>
2025-02-18 18:55:22 -0800

    add comments to to_hex accessor

--- odf/sbe/accessors.py
+++ odf/sbe/accessors.py
@@ -23,32 +23,48 @@ class SBEAccessor():
         self._obj = xarray_object
 
     def to_hex(self, path: str|PathLike|None = None, check=True):
+        """return or write a .hex file identical to the input of read_hex
+        
+        The output will not be identical if the errors of read_hex was not "store".
+        """
+        # extract the two relivant DataArrays
+        # Note that the scan coordinate will be carried with these by xarray
         _hex = self._obj.hex
         _hex_errors = self._obj.get("hex_errors")
 
+        # If there are errors rows, construct a dict that maps scan count to hex string
+        # Also update the total scan to be the sum of normal scans and error scans
         error_rows = {}
         total_scans = _hex.sizes["scan"]
         if _hex_errors is not None:
             total_scans += _hex_errors.sizes["scan_errors"]
             error_rows = dict(zip(_hex_errors.scan_errors.values.tolist(), _hex_errors.values.tolist(), ))
 
+        # just prepare the header
         header = "\r\n".join(_hex.attrs["header"].splitlines())
-        hex_data = bytes(_hex.as_numpy().values).hex().upper()
     
+        # construct a dict that maps scan count to hex string
+        # the hex_data is made as one big string all at once (very fast)
+        # so the index/pointer math quickly slices this into rows
         data_rows = []
         scans = _hex.scan.values
         row_len = _hex.sizes["bytes_per_scan"] * 2
+        hex_data = bytes(_hex.as_numpy().values).hex().upper()
         for row in range(_hex.sizes["scan"]):
             start = row * row_len
             stop = row * row_len + row_len
             data_rows.append((scans[row].item(), hex_data[start:stop]))
 
+        # Chain map will check the dicts in order for the presence of the key
+        # data out is the "final" list of hex strings that will be joined by the line seperator
         data_dict = ChainMap(dict(data_rows), error_rows)
         data_out = []
         for row in range(total_scans):
             scan = row + 1
             data_out.append(data_dict[scan])
+        # The final "" here makes an empty line at the end of the file
         data_out = "\r\n".join([header, *data_out, ""]).encode(_hex.attrs.get("charset", "utf8"))
+        # do the output check but only if there is a Content-MD5 attr
         if check and (filehash := _hex.attrs.get("Content-MD5")) is not None:
             digest = md5(data_out).hexdigest()
             if digest != filehash:
