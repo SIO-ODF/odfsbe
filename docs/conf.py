b97e00c5fc8d91404a9963d92d69d2fcd978d437
Andrew Barna <abarna@gmail.com>
2025-02-26 09:28:46 -0800

    ran ruff format

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'odf.sbe'
-copyright = '2025, odf.sbe Contributors'
-author = 'odf.sbe Contributors'
+project = "odf.sbe"
+copyright = "2025, odf.sbe Contributors"
+author = "odf.sbe Contributors"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -16,8 +16,8 @@ author = 'odf.sbe Contributors'
 extensions = ["myst_parser", "autoapi.extension"]
 myst_enable_extensions = ["colon_fence"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 autoapi_dirs = ["../src/odf"]
@@ -25,5 +25,5 @@ autoapi_dirs = ["../src/odf"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
